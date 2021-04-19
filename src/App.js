475750a2be4502b23cc58933d285048c68e5f5e4
Uzair Munir <uzair8825@gmail.com>
2021-04-19 19:51:47 +0500

    Infinte scroll added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
-
-function App() {
+const App = () => {
+  const [photos, setPhotos] = useState([]);
+  const [pageNumber, setPageNumber] = useState(1);
+  const fetchPhotos = async (pageNumber) => {
+    const url = await fetch(
+      `https://picsum.photos/v2/list?page=${pageNumber}&limit=10`
+    );
+    const data = await url.json();
+    setPhotos((photo) => [...photo, ...data]);
+  };
+  useEffect(() => {
+    fetchPhotos(pageNumber);
+  }, [pageNumber]);
+  console.log(photos);
+  const loadMore = (e) => {
+    e.preventDefault();
+    setPageNumber((prev) => prev + 1);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <h1>Infinite Scroll</h1>
+      {photos.map((photo) => (
+        <div key={photo.id} className='photo-container'>
+          <img src={photo.download_url} className='photos' />
+          <p>{photo.author}</p>
+        </div>
+      ))}
+      <p>Length : {photos.length}</p>
+      <button className='btn' onClick={loadMore}>
+        Load More
+      </button>
     </div>
   );
-}
+};
 
 export default App;
